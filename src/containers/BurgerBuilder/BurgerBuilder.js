bea87ee5cbf79d701800cff321dde8fbf9e4bddf
Hands34 <m.cook2434@gmail.com>
2021-01-15 15:11:52 -0600

    added functionality for the events of add ingredient and remove ingredient to updated the state and show updated burger on page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -58,7 +58,8 @@ class BurgerBuilder extends Component {
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls 
-                    ingredientAdded={this.addIngredientHandler}/>
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}/>
             </Aux>
         );
     }
