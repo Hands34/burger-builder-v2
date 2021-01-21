7d46cc852234c74b2692e6ead08c644a1ed8ba55
Hands34 <m.cook2434@gmail.com>
2021-01-21 14:04:31 -0600

    added a spinner to the modal to show something while the order post request is processing

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import classes from './Modal.module.css';
 import Aux from '../../../hoc/Aux/Aux';
 import Backdrop from '../Backdrop/Backdrop';
+//import Spinner from '../Spinner/Spinner';
 
 class Modal extends Component {
 
     shouldComponentUpdate (nextProps, nextState) {
-        return nextProps.show !== this.props.show;
+        return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
     }
 
     componentWillUpdate() {
